fc97b31722df8e1a4940eefe8e86b4779388de34
Victor Chen <chen116@usc.edu>
2023-07-30 20:28:17 +0700

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  let cnt = 0;
+  function increment() {
+		cnt += 1;
+	}
+</script> 
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={increment}>add 1</button>
+<h1>{cnt}</h1>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
