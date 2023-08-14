cb2b993e96eaf06da5d2f03cb8b3b902bfe62930
Victor Chen <cyavictor88@gmail.com>
2023-08-14 08:31:23 +0700

    chart up y limit need to fs now

--- src/routes/signalProcessing/fourierSeries/intro/+page.svelte
+++ src/routes/signalProcessing/fourierSeries/intro/+page.svelte
@@ -5,45 +5,26 @@
 	import { getContext } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	import Katex from '$lib/components/Katex/Katex.svelte';
-  import Chart from 'chart.js/auto'
-	import { data } from './chart/data';
+	import { makeChart } from './chart/chart';
 	const innerPageRoute = getContext<Writable<IRoute>>('innerPageRoute');
-  let exmapleFunctionCanvas : HTMLCanvasElement;
+	let exmapleFunctionCanvas: HTMLCanvasElement;
 	onMount(() => {
 		innerPageRoute.set(pageRoute);
-    new Chart(exmapleFunctionCanvas, {
-    type: 'scatter',
-    data: {
-      datasets: [{
-        label: 'f(x) = x^3 - 12x + 16',
-        data: data(),
-        borderWidth: 1,
-        pointStyle: false,
-        showLine: true,
-        borderColor: 'red',
-        backgroundColor: 'red'
-      }]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-
-
-    }
-  });
+    const chart = makeChart(exmapleFunctionCanvas);
 	});
 </script>
 
 <h1>Fourier Series - Introduction</h1>
 <p>
-	Fourier Series is a method to decompose a given function <Katex math={'f(x)'} /> into a combination of <Katex math={'sin'} /> and <Katex math={'cos'}/> functions.
+	Fourier Series is a method to decompose a given function <Katex math={'f(x)'} /> into a combination
+	of <Katex math={'sin'} /> and <Katex math={'cos'} /> functions.
 </p>
 <p>Example:</p>
 <Katex math={'f(x) = x^3 -12 x +16'} />
-<div style='width:500px; height:200px;'>
-<canvas bind:this={exmapleFunctionCanvas} />
+<div style="max-width: 500px; height:200px;">
+	<canvas bind:this={exmapleFunctionCanvas} />
 </div>
-<p>We can decompose <Katex math={'f(x)'} /> into </p>
+<p>We can decompose <Katex math={'f(x)'} /> into</p>
 <p>Say we have a function</p>
 <p>
 	To understand Fourier Series, we will first go over inner product. With inner product, we can
---
+++ src/routes/signalProcessing/fourierSeries/intro/chart/chart.ts
@@ -0,0 +1,47 @@
+import Chart from "chart.js/auto";
+
+function linspace(startValue:number , stopValue:number, cardinality:number) {
+  const arr = [];
+  const step = (stopValue - startValue) / (cardinality - 1);
+  for (let i = 0; i < cardinality; i++) arr.push(startValue + (step * i));
+  return arr;
+}
+
+
+export function data():{x:number,y:number}[] {
+  const xs = linspace(-5,5,100);
+  const ys = xs.map(x=> x**3 -12*x +16)// = (x-2)**2 * (x+4)*1 ;
+  const arr = [];
+  for (let i = 0; i < xs.length; i++) arr.push({x:xs[i],y:ys[i]});
+  return arr
+}
+
+export function makeChart(ctx:HTMLCanvasElement) {
+  const chartData = data();
+  return new Chart(ctx, {
+    type: 'scatter',
+    data: {
+      datasets: [
+        {
+          label: 'f(x) = x^3 - 12x + 16',
+          data: chartData,
+          borderWidth: 1,
+          pointStyle: false,
+          showLine: true,
+          borderColor: 'red',
+          backgroundColor: 'red'
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y :{
+          min:chartData[0].y,
+          max:chartData[chartData.length-1].y,
+        }
+        }
+    }
+  });
+}
