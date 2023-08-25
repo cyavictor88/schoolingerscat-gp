6007cb6c5c500f2d7fe9f2cb11a87794b8c2616f
Victor Chen <cyavictor88@gmail.com>
2023-08-25 07:09:21 +0700

    todo: arrow d3

--- src/routes/d3/SvgD3.svelte
+++ src/routes/d3/SvgD3.svelte
@@ -1,12 +1,12 @@
 <script lang='ts'>
 	import { onMount } from "svelte";
-	import { svg1,svg2,svg3 } from "./svgs";
+	import { svgF1,svg2,svg3 } from "./svgs";
 
   let div1: HTMLDivElement;
   let div2: HTMLDivElement;
   let div3 : HTMLDivElement;
   onMount(()=>{
-    div1.append(svg1()!);
+    div1.append(svgF1()!);
     div2.append(svg2()!);
     div2.append(svg3()!);
   })
--- src/routes/d3/svgs.ts
+++ src/routes/d3/svgs.ts
@@ -1,6 +1,138 @@
 import * as d3 from 'd3';
 import katex from "katex";
 
+
+
+
+
+
+
+export function svgF1() {
+  // Declare the chart dimensions and margins.
+  const width = 300;
+  const height = 300;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+
+  const xDomain = [0,5.5];
+  const yDomain = [0,5.5];
+  // Declare the x (horizontal position) scale.
+  const xScale = d3.scaleLinear()
+    .domain(xDomain)
+    .range([marginLeft, width - marginRight]);
+
+  // Declare the y (vertical position) scale.
+  const yScale = d3.scaleLinear()
+    .domain(yDomain)
+    .range([height - marginBottom, marginTop]);
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // Add the x-axis.
+  // svg.append("g")
+  //   .attr("transform", `translate(0,${height - marginBottom})`)
+  //   .call(d3.axisBottom(x).tickValues([]));
+
+  // // Add the y-axis.
+  // svg.append("g")
+  //   .attr("transform", `translate(${marginLeft},0)`)
+  //   .call(d3.axisLeft(y).tickValues([]));
+
+  function drawPath(context: d3.Path) {
+    context.moveTo(10, 10); // move current point to ⟨10,10⟩
+    context.lineTo(100, 10); // draw straight line to ⟨100,10⟩
+    context.arcTo(150, 150, 300, 10, 40); // draw an arc, the turtle ends up at ⟨194.4,108.5⟩
+    context.lineTo(300, 10); // draw straight line to ⟨300,10⟩
+    // etc.
+    return context; // not mandatory, but will make it easier to chain operations
+  }
+
+  const drawLine = d3.line<{x:number,y:number}>()
+  .x(d => xScale(d.x))
+  .y(d => yScale(d.y))
+
+  // svg.append('path')
+  //   .style("stroke", "black")
+  //   .style("fill", "none")
+  //   .attr("d", drawPath(d3.path()).toString())
+
+    svg.append('path')
+    .style("stroke", "black")
+    .style("fill", "none")
+    .attr("d", drawLine([{x:0,y:0},{x:5,y:0}]))
+
+    svg.append('path')
+    .style("stroke", "black")
+    .style("fill", "none")
+    .attr("d", drawLine([{x:0,y:0},{x:0,y:5}]))
+
+
+    const fontSize = 20;
+    const textXaxis = svg.append("text")
+    .attr("x", xScale(5))
+    .attr("y", yScale(-0))
+    .style("font-size", fontSize + "px")
+    .attr("dy", yScale.invert(fontSize * 0.5))
+    .style('fill','black')
+    .attr("text-anchor", "start")
+    .text("X");
+
+    const textYaxis = svg.append("text")
+    .attr("x", xScale(0))
+    .attr("y", yScale(5.1))
+    .style("font-size", fontSize + "px")
+    .style('fill','black')
+    .attr("text-anchor", "middle")
+    .text("Y");
+    // textElement.attr("transform", `translate(0, ${textHeight / 2}px)`);
+
+
+  svg.selectAll('text').style('fill', 'black')
+
+
+  const markerBoxWidth = 20;
+  const markerBoxHeight = 20;
+  const refX = markerBoxWidth/2;
+  const refY = markerBoxHeight/2;
+  const arrowPoints :[number,number][]= [[0, 0], [0, 20], [20, 10]];
+
+
+  const arrowLine = d3.line<[number,number]>().x(d=>(d[0])).y(d=>(d[0]));
+
+  svg
+    .append('defs')
+    .append('marker')
+    .attr('id', 'arrow')
+    .attr('viewBox', [0, 0, markerBoxWidth, markerBoxHeight])
+    .attr('refX', refX)
+    .attr('refY', refY)
+    .attr('markerWidth', markerBoxWidth)
+    .attr('markerHeight', markerBoxHeight)
+    .attr('orient', 'auto-start-reverse')
+    .append('path')
+    .attr('d', arrowLine(arrowPoints))
+    .attr('fill', 'black')
+    .attr('stroke', 'black');
+
+
+    svg
+    .append('path')
+    .attr('d', drawLine([{x:0,y:0},{x:3,y:3}]))
+    .attr('stroke', 'black')
+    .attr('marker-end', 'url(#arrow)')
+    .attr('fill', 'none');
+
+
+  return svg.node();
+}
+
+
 export function svg3() {
   // Declare the chart dimensions and margins.
   const width = 640;
@@ -143,7 +275,7 @@ export function svg2() {
       k = k2 + a * k1;
     }
 
-    const mathmlHtml = katex.renderToString(`\\frac{${h}}{${k}}`, {
+    const mathmlHtml = katex.renderToString(`\\theta\\frac{${h}}{${k}}`, {
       throwOnError: false,
       output: "mathml",
       displayMode: false,
