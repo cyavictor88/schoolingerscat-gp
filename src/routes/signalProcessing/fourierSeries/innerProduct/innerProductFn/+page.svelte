c3d0e2104a939949b960dbb021001e75e453f58f
Victor Chen <cyavictor88@gmail.com>
2023-09-11 09:39:03 +0700

    todo:inner product fn

--- src/routes/signalProcessing/fourierSeries/innerProduct/innerProductFn/+page.svelte
+++ src/routes/signalProcessing/fourierSeries/innerProduct/innerProductFn/+page.svelte
@@ -7,4 +7,23 @@
 <Title hLevel={2} backgroudColor={SITE_COLOR.TitleBG}>
   Inner Product for Functions:
 </Title>
-<p>Haro</p>
+<p>Using the definition of Inner Product for complex vectors in n-dimensional space, 
+  we can manufacture Inner Product for Functions with complex number. 
+  Such function space is refered as <a href='https://en.wikipedia.org/wiki/Hilbert_space'>Hilbert space</a>.</p>
+
+
+<strong>Inner Product for Functions:</strong>
+
+<Latex displayMode={true} math={`
+    \\langle f(t) , g(t) \\rangle = \\int_{t_1}^{t_2} f(t) g^*(t) \\,dt
+  `}
+/>
+
+<span> (Where * is complex conjugate)</span>
+
+<p>Now let's explain two things: </p>
+<ol>
+  <li>Why is <Latex math={'\\sum_{}^{}'} /> replaced by <Latex math={'\\int_{}^{}'} />?</li>
+  <li>What are the new variables  <Latex math={'t_1'} /> and <Latex math={'t_2'} /> doing here?</li>
+
+</ol>
