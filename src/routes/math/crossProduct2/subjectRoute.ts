2372e1afebfd08a76e58fc982f37eb2380db48bb
Victor Chen <cyavictor88@gmail.com>
2023-12-30 07:27:04 +0700

    todo: combine cross produt and determinant

--- src/routes/math/crossProduct2/subjectRoute.ts
+++ src/routes/math/crossProduct2/subjectRoute.ts
@@ -3,9 +3,9 @@ import type { IRoute } from "$lib/components/Route/route";
 export const subjectRoute : IRoute = {
   label: 'Cross Product2',
   subRoutes: [
-    { label: 'Introduction', path: '/math/crossProduct/introduction'},
-    { label: 'Parallelepiped', path: '/math/crossProduct/parallelepiped'},
-    { label: 'Linear Functional', path: '/math/crossProduct/linearFunctional'},
-    { label: 'Cross Product Proof', path: '/math/crossProduct/crossProductProof'},
+    { label: 'Introduction', path: '/math/crossProduct2/introduction'},
+    { label: 'Parallelepiped', path: '/math/crossProduct2/parallelepiped'},
+    { label: 'Linear Functional', path: '/math/crossProduct2/linearFunctional'},
+    { label: 'Cross Product Proof', path: '/math/crossProduct2/crossProductProof'},
   ]
 }
