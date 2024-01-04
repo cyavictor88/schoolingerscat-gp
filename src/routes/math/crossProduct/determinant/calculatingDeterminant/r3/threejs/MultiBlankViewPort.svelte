8b8cfcda8b3a93d9856f57a1b9da9cab9f701524
Victor Chen <cyavictor88@gmail.com>
2024-01-04 08:33:08 +0700

    todo: explore multi viewPort

--- src/routes/math/crossProduct/determinant/calculatingDeterminant/r3/threejs/MultiBlankViewPort.svelte
+++ src/routes/math/crossProduct/determinant/calculatingDeterminant/r3/threejs/MultiBlankViewPort.svelte
@@ -1,36 +1,39 @@
 <script lang='ts'>
 	import { onMount, afterUpdate } from "svelte";
 	import Toggle from "../../Toggle.svelte";
+	import { Multiverse } from './Multiverse';
+	import { Universe as Universe1 } from './Universe';
 
 	let grandDiv: HTMLDivElement;
-	let canvasHeight: number = 0;
+	let grandDivH: number = 0;
   onMount(()=>{
-    canvasHeight = grandDiv.clientHeight;
+    grandDivH = grandDiv.clientHeight;
 
   })
   $: {
-    console.log('canvasHeight',canvasHeight)
+    console.log('grandDivH',grandDivH)
   }
   let offsetHeight = 0;
   let offsetWidth=0;
   let offsetLeft=0;
   let offsetTop=0;
+  let canvasH = 0;
 
   let uni1Div: HTMLDivElement;
-  let map_offset = 0;
+  let uni1OffsetTop = 0;
   afterUpdate(() => {
-  map_offset = uni1Div.offsetTop;
-});
+    uni1OffsetTop = uni1Div.offsetTop;
+  });
 </script>
 
-<p>offsetHeight{offsetHeight},offsetWidth{offsetWidth},map_offset{map_offset},ch,{canvasHeight}</p>
+<p>offsetHeight{offsetHeight},offsetWidth{offsetWidth},uni1OffsetTop{uni1OffsetTop},gdh,{grandDivH},ch,{canvasH}</p>
 {#if uni1Div}
-<p>offsetHeight{uni1Div.clientHeight},offsetWidth{uni1Div.offsetWidth},top{uni1Div.offsetTop},left{uni1Div.offsetLeft}</p>
+<p>uni1Div: offsetHeight{uni1Div.clientHeight},offsetWidth{uni1Div.offsetWidth},top{uni1Div.offsetTop},left{uni1Div.offsetLeft}</p>
 {/if}
-<div bind:this={grandDiv} bind:clientHeight={canvasHeight}
+<div bind:this={grandDiv} bind:clientHeight={grandDivH}
 	style="position: relative; width:1004px; display:flex; flex-flow: row wrap; border: 1px solid blue"
 >
-<canvas  />
+<canvas bind:offsetHeight={canvasH} />
 <div style="position: relative;">
   <Toggle>
     <div  bind:offsetHeight={offsetHeight} bind:offsetWidth={offsetWidth}>{offsetHeight},{offsetWidth}</div>
