f6b8464d2bb8eda5ef13b04136e0b97d927cd7f6
Victor Chen <cyavictor88@gmail.com>
2024-01-07 22:02:31 +0700

    todo: d3 : cross product proof

--- src/routes/math/crossProduct/crossProductProof/+page.svelte
+++ src/routes/math/crossProduct/crossProductProof/+page.svelte
@@ -4,9 +4,46 @@
 	import Title from '$lib/components/PageComp/Title.svelte';
 	import Latex from '$lib/components/Latex/Latex.svelte';
 	import ColsVec from '../common/latex/ColsVec.svelte';
+  import { page } from '$app/stores';
+	import {VolumeAreaHeight} from './d3/VolumeAreaHeight';
+		const vecv = [2, 3, -1];
+		const veca = [-4, -1, -1];
+		const vecb = [-3, -1, 3];
+		// const rowop = mj.add(mj.multiply(2, vecv), vecb) as number[];
+		// let d3 : VolumeAreaHeight = new VolumeAreaHeight();
 	onMount(() => {
 
 	});
 </script>
 
 <Title hLevel={1}>Cross Product - Proof</Title>
+
+<p>From the previous <a href={$page.route.id+'/../linearFunctional'}>Linear Functional</a>, we know
+
+</p>
+<Latex center math={` \\phi ( \\vec{v} ) = 
+			               det \\begin{bmatrix}
+              v_{1}  & v_{2} &  v_{3} \\\\
+             a_{1} & a_{2} & a_{3} \\\\
+              b_{1} & b_{2} & b_{3} \\\\
+              \\end{bmatrix} =
+			\\vec{p} \\cdot \\vec{v}` }/> 
+			<Latex center math={'\\text{ where } \\vec{p} \\text{ is unique,}  \\vec{v}= [v_1,v_2,v_3] \\text{ is variable vector}'} />
+			<Latex center math={'\\text{and } \\vec{a}=[a_1,a_2,a_3], \\vec{b}=[b_1,b_2,b_3] \\text{ are two constant vectors}'} />
+
+<p>We need to prove that 
+</p>
+<ul>
+	<li><Latex math={'\\vec{p}'} /> is orthogonal to the Area formed by <Latex math={'\\vec{a}'} />, <Latex math={'\\vec{b}'} /> </li>
+	<li><Latex math={'\\|\\vec{p}\\|=' } />  Area formed by <Latex math={'\\vec{a}'} />, <Latex math={'\\vec{b}'} /> </li>
+</ul>
+<hr />
+<p><b>Proof:</b></p>
+
+<Latex center math={`
+			              \\left|\\left| det \\begin{bmatrix}
+              v_{1}  & v_{2} &  v_{3} \\\\
+             a_{1} & a_{2} & a_{3} \\\\
+              b_{1} & b_{2} & b_{3} \\\\
+              \\end{bmatrix} \\right| \\right| = Volume
+			(\\vec{v},\\vec{a},\\vec{b}) = Area_{\\vec{a}\\vec{b}} * height` }/> 
--- src/routes/math/crossProduct/linearFunctional/+page.svelte
+++ src/routes/math/crossProduct/linearFunctional/+page.svelte
@@ -15,13 +15,17 @@
 <p>In this section, we will show that</p>
 <dl>
   <dt><b>{getCircleNum(1)}:</b></dt>
-  <dd>If we fix <Latex math={`\\vec{a} =`} /><ColsVec cols={['a']} dim={3} />
+  <dd>If we fix <Latex math={`\\vec{a} = [a_1,a_2,a_3]`} />
 		and
-		<Latex math={`\\vec{b} =`} /><ColsVec cols={['b']} dim={3} /> as constants, and make <Latex
-			math={`\\vec{v} =`}
-		/><ColsVec cols={['v']} dim={3} /> as a variable vector. we then can create a linear functional
-		<Latex math={'\\phi ( \\vec{v} ):= \\det'} />
-		<RowsVec cols={['v', 'a', 'b']} dim={3} />
+		<Latex math={`\\vec{b} = [b_1,b_2,b_3]`} /> as constants, and make <Latex
+			math={`\\vec{v} =[v_1,v_2,v_3]`}
+		/> as a variable vector. we then can create a linear functional
+		<Latex center math={`\\phi ( \\vec{v} ):= \\det \\begin{bmatrix}
+		v_{1}  & v_{2} &  v_{3} \\\\
+	 a_{1} & a_{2} & a_{3} \\\\
+		b_{1} & b_{2} & b_{3} \\\\
+		\\end{bmatrix}`} />
+		
 	</dd>
   <dt><b>{getCircleNum(2)}:</b></dt>
   <dd>Then we can show that there is a unique vector, <Latex math={`\\vec{p}`} />, such that <Latex
@@ -50,8 +54,14 @@
 		From the fact that <Latex math={'\\phi ( \\vec{v} )=  p_1 v_1 + p_2 v_2 + p_3 v_3  \\text{ where } p_i \\in \\mathbb{R}'} />:
 		<li>
 			By using the formula of inner product:
-			<Latex center math={'\\phi ( \\vec{v} ) = p_1 v_1 + p_2 v_2 + p_3 v_3 = \\vec{p} \\cdot \\vec{v} \\text{ where } \\vec{p} = [p_1,p_2,p_3]^T ' }/> 
-			<Latex center math={'\\Rightarrow \\phi ( \\vec{v} ) = \\vec{p} \\cdot \\vec{v}' }/> 
+			<Latex center math={'\\phi ( \\vec{v} ) = p_1 v_1 + p_2 v_2 + p_3 v_3 = \\vec{p} \\cdot \\vec{v} \\text{ where } \\vec{p} = [p_1,p_2,p_3] ' }/> 
+			<Latex center math={`\\Rightarrow \\phi ( \\vec{v} ) = 
+			               det \\begin{bmatrix}
+              v_{1}  & v_{2} &  v_{3} \\\\
+             a_{1} & a_{2} & a_{3} \\\\
+              b_{1} & b_{2} & b_{3} \\\\
+              \\end{bmatrix} =
+			\\vec{p} \\cdot \\vec{v}\\text{ ,where } \\vec{v}= [v_1,v_2,v_3]` }/> 
 
 		</li>
 
